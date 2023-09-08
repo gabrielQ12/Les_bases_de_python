b27ea662d55f2fc4a042c25e36f9cc80a8d3baa3
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-09-08 07:47:23 -0400

    casseur de mdp fonnction recursive AU PROPRE

--- password_cracker/crack.py
+++ password_cracker/crack.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env/python3
 
-import random
 import time
 import string
 import hashlib
@@ -8,21 +7,16 @@ import sys
 import argparse
 import atexit
 
-def display_name():
-    print("Durée :" + str(time.time() - debut) + "second")
 
-
-parser = argparse.ArgumentParser(description="Password Cracker")
-
-parser.add_argument("-f", "--file", dest="file", help="Path of dictionary file", required=False)
-parser.add_argument("-g", "--gen", dest="gen", help="Generate MD5 hash of password", required=False)
-parser.add_argument("-md5", dest="md5", help="Hashed password (MD5)", required=False)
-parser.add_argument("-l", dest="plength", help="Password length", required=False, type=int)
-
-args = parser.parse_args()
+class Couleur:
+    ROUGE= '\033[91m'
+    VERT = '\033[92m'
+    ORANGE = '\033[93m'
+    FIN = '\033[0m'
 
 
 def crack_dict(md5, file):
+    #Casse un Hash MD5 via une liste de mots-clé (file)
     try:
         trouve = False
         ofile = open(file, "r")
@@ -30,20 +24,21 @@ def crack_dict(md5, file):
             mot = mot.strip("\n")
             hashmd5 = hashlib.md5(mot.encode("utf8")).hexdigest()
             if hashmd5 == md5:
-                print("Mot de passe trouvé : " + str(mot) + "(" + hashmd5 + ")")
+                print(Couleur.VERT + "[+] MOT DE PASSE TROUVE: " + str(mot) + "(" + hashmd5 + ")" + Couleur.FIN)
                 trouve = True
         if not trouve:
-            print("Mot de passe non trouvé")
+            print(Couleur.ROUGE + "[-] MoOT DE PASSE NON TROUVE :(" + Couleur.FIN)
         ofile.close()
     except FileNotFoundError:
-        print("Erreur : nom de dossier ou fichier introuvable")
+        print(Couleur.ROUGE + "[-] ERREUR : nom de dossier ou fichier introuvable ! " + Couleur.FIN)
         sys.exit(1)
     except Exception as err:
-        print("Erreur : " + str(err))
+        print(Couleur.ROUGE + " [-] Erreur : " + str(err) + Couleur.FIN)
         sys.exit(2)
 
 
 def crack_incr(md5, length, currpass=[]):
+    #Casse un Hash MD5 via une méthode incrémentale pour un mdp de longueur "length"
     lettres =string.ascii_letters
     if length >=1:
         if len(currpass) == 0:
@@ -52,29 +47,49 @@ def crack_incr(md5, length, currpass=[]):
     else:
         for c in lettres:
             currpass[length - 1] = c
-            print("Trying : " + "".join(currpass))
-            if hashlib.md5("".join(currpass).encode("utf8")).hexdigest() == md5:
-                print("PASSWORD FOUND ! " + "".join(currpass))
+            currhash = hashlib.md5("".join(currpass).encode("utf8")).hexdigest()
+            print("[*] TEST DE : " + "".join(currpass)+ "(" + currash + ")")
+            if currhash == md5
+                print(Couleur.VERT + "[+] MOT DE PASSE TROUVE : " + "".join(currpass) + Couleur.FIN)
                 sys.exit(0)
             else:
                 crack_incr(md5,length - 1,currpass)
+else:
+    return
+
+def affiche_duree():
+    #Affiche la durée écoulée à la fin du script
+    print("Durée écoulée :" + str(time.time() - debut) + "second")
+
+
+parser = argparse.ArgumentParser(description="Casseur de mot de passe en Python)
+
+parser.add_argument("-f", "--file", dest="file", help="PChemin du fichier de mots clés" , required=False)
+parser.add_argument("-g", "--gen", dest="gen", help="Génère un hash MD5 du mot de passe donné", required=False)
+parser.add_argument("-md5", dest="md5", help="Mot de passe MD5 à casser", required=False)
+parser.add_argument("-l", dest="plength", help="Longueur du mot de passe (mode incrémental seulement)", required=False, type=int)
+
+args = parser.parse_args()
 
 debut = time.time()
-atexit.register(display_name)
+atexit.register(affiche_duree)
+
+
+if args.gen:
+    print("[*] HASH MD5 DE" + args.gen + "=" + hashlib.md5(args.gen.encode("utf8")).hexdigest())
 
 if args.md5:
-    print("[CRACKING HASH" + args.md5 + "]")
-    if args.file and not args.plength:
-        print("USING DICTIONARY FILE " + args.file)
+    print("[*] [CRACKING DU HASH" + args.md5 )
+    if args.file:
+        print("[*] UTILISANT LE FICHIER DE MOTS-CLES " + args.file)
         crack_dict(args.md5, args.file)
-    elif args.plength and not args.file:
-        print("USING INCREMENTAL MODE FOR" + str(args.plength) + " letter(s)")
+    elif args.plength:
+        print("[*] UTILISANT LE MODE INCREMENTAL POUR " + str(args.plength) + "LETTRE(S)")
         crack_incr(args.md5, args.plength)
     else:
-        print("Please choose either -f or -l argument")
+        print(Couleur.ROUGE + "[-] PVEUILLEZ CHOISIR L'ARGUMENT -f OU -l avec -md5." + Couleur.FIN)
 else :
-    print("MD5 hash not provided")
-if args.gen:
-    print("[MD5 HAS OF" + args.gen + ":" + hashlib.md5(args.gen.encode("utf8")).hexdigest())
+    print(Couleur.ROUGE + "[-] HASH MD5 NON FOURNI." + Couleur.FIN)
+
 
 
