f24c6c4d467c7c1b80df49ee6bdb308d2ff66283
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-09-08 08:42:40 -0400

    casseur de mdp avec recherche en ligne

--- password_cracker/crack.py
+++ password_cracker/crack.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env/python3
-
 import time
 import string
 import hashlib
 import sys
 import argparse
 import atexit
+import urllib.request
+import urllib.response
+import urllib.error
 
 
 class Couleur:
@@ -17,6 +19,7 @@ class Couleur:
 
 def crack_dict(md5, file):
     #Casse un Hash MD5 via une liste de mots-clé (file)
+
     try:
         trouve = False
         ofile = open(file, "r")
@@ -39,35 +42,56 @@ def crack_dict(md5, file):
 
 def crack_incr(md5, length, currpass=[]):
     #Casse un Hash MD5 via une méthode incrémentale pour un mdp de longueur "length"
-    lettres =string.ascii_letters
-    if length >=1:
+
+    lettres = string.ascii_letters
+    if length >= 1:
         if len(currpass) == 0:
             currpass = ['a' for _ in range(length)]
             crack_incr(md5,length, currpass)
-    else:
-        for c in lettres:
-            currpass[length - 1] = c
-            currhash = hashlib.md5("".join(currpass).encode("utf8")).hexdigest()
-            print("[*] TEST DE : " + "".join(currpass)+ "(" + currash + ")")
-            if currhash == md5
-                print(Couleur.VERT + "[+] MOT DE PASSE TROUVE : " + "".join(currpass) + Couleur.FIN)
-                sys.exit(0)
+        else:
+            for c in lettres:
+                currpass[length - 1] = c
+                currhash = hashlib.md5("".join(currpass).encode("utf8")).hexdigest()
+                print("[*] TEST DE : " + "".join(currpass)+ "(" + currash + ")")
+                if currhash == md5:
+                    print(Couleur.VERT + "[+] MOT DE PASSE TROUVE : " + "".join(currpass) + Couleur.FIN)
+                    sys.exit(0)
             else:
                 crack_incr(md5,length - 1,currpass)
-else:
-    return
+    else:
+        return
+
+
+def crack_en_ligne(md5):
+    try:
+        agent_utilisateur = "Mozilla/5.0 (Windows; U; Windows NT 5.1;fr-FR; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
+        headers = {'User-Agent':agent_utilisateur}
+        url = "https://www.google.fr/search?hl=fr&q=" + md5
+        requete = urllib.request.Request(url, None, headers)
+        reponse = urllib.request.urlopen(requete)
+    except urllib.error.HTTPError as e:
+        print("Erreur HTTP : " + e.code)
+    except urllib.error.URLError as e:
+        print("Erreur d'URL : ", e.reason)
+
+    if "Aucun document" in str(reponse.read()):
+        print(Couleur.ROUGE, "[-] HASH TROUVE VIA GOOGLE" + Couleur.FIN)
+    else:
+        print(Couleur.VERT,"[+] MOT DE PASSE TROUVE VIA GOOGLE : " + url + Couleur.FIN)
+
 
 def affiche_duree():
     #Affiche la durée écoulée à la fin du script
     print("Durée écoulée :" + str(time.time() - debut) + "second")
 
 
-parser = argparse.ArgumentParser(description="Casseur de mot de passe en Python)
+parser = argparse.ArgumentParser(description="Casseur de mot de passe en Python")
 
 parser.add_argument("-f", "--file", dest="file", help="PChemin du fichier de mots clés" , required=False)
 parser.add_argument("-g", "--gen", dest="gen", help="Génère un hash MD5 du mot de passe donné", required=False)
 parser.add_argument("-md5", dest="md5", help="Mot de passe MD5 à casser", required=False)
 parser.add_argument("-l", dest="plength", help="Longueur du mot de passe (mode incrémental seulement)", required=False, type=int)
+parser.add_argument("-o", dest="online", help="Cherche le Hash en ligne (google)", required=False, action="store_true")
 
 args = parser.parse_args()
 
@@ -86,6 +110,9 @@ if args.md5:
     elif args.plength:
         print("[*] UTILISANT LE MODE INCREMENTAL POUR " + str(args.plength) + "LETTRE(S)")
         crack_incr(args.md5, args.plength)
+    elif args.online:
+        print("[*] UTILISANT LE MODE EN LIGNE ")
+        crack_en_ligne(args.md5)
     else:
         print(Couleur.ROUGE + "[-] PVEUILLEZ CHOISIR L'ARGUMENT -f OU -l avec -md5." + Couleur.FIN)
 else :
